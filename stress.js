3066cce9fc018d251a0dfaea537b961e93aca154
Wasser <wasserx@hotmail.com>
2012-06-05 11:12:53 +0200

    Compatible with Hubiquitus 0.3

--- stress.js
+++ stress.js
@@ -16,61 +16,73 @@
  *     You should have received a copy of the GNU General Public License
  *     along with Hubiquitus.  If not, see <http://www.gnu.org/licenses/>.
  */
-var options = { };
-var winston = require('winston');
-var hSession  = require("./lib/hsession.js");
+var logger = require('winston');
+var hClient  = require("hubiquitusjs");
 var nbOpenedSessions = 0;
 var openedSessions = new Array();
 
-/*
-Each session is launched asynchronously
+/**
+ * Callback executed by each session
  */
-function launch(opt){
+var hCallback = function(sessionNb, msg){
+    if (msg.type == 'hStatus' && msg.data.status == hClient.status.CONNECTED)
+        logger.info('Session #' + sessionNb + ' connected');
+};
 
-    var session = new hSession(opt);
-    session.connect();
-    openedSessions[nbOpenedSessions] = session;
-    nbOpenedSessions++;
-    winston.info('Opening session # '+ nbOpenedSessions);
+/*
+ Each session is launched asynchronously
+ */
+function launch(opts, hOptions){
 
-    if (opt.workers>nbOpenedSessions){
-        setTimeout(function() { launch(opt) } , 5000);
-    }
+    var session = new hClient.HubiquitusClient();
+    session.connect(opts.username, opts.password, hCallback.bind(session, nbOpenedSessions), hOptions);
 
+    logger.info('Opening session #'+ nbOpenedSessions);
+    openedSessions[nbOpenedSessions++] = session;
+
+    if (opts.sessions>nbOpenedSessions)
+        setTimeout(function() { launch(opts, hOptions) } , opts.interval);
 }
 
 function main() {
 
-    winston.handleExceptions();
+    //logger.handleExceptions();
 
-	var opts = require('tav').set({
-		username: {
-			note: 'The username to login as'
-		} 
-		,password: {
-			note: 'The password to use'
-		} 
-		,endpoint: {
-			note: 'The BOSH service endpoint (default: http://localhost:5280/http-bind/)', 
-			value: 'http://localhost:5280/http-bind/'
-		}
-		,node: {
-			note: 'The name of the node to suscribe to'
-		} 
-		,route: {
-			note: 'The route attribute to use (default: <empty>)', 
-			value: ''
-		}
-		,sessions: {
-			note: 'The number of sessions to launch (default: 2)',
-			value: 2
-		}
-	});
+    var opts = require('tav').set({
+        username: {
+            note: 'The username to login as'
+        }
+        ,password: {
+            note: 'The password to use'
+        }
+        ,endpoint: {
+            note: 'The hNode Socket.IO endpoint (default: http://localhost:8080/)',
+            value: 'http://localhost:8080/'
+        }
+        ,sessions: {
+            note: 'The number of sessions to launch (default: 2)',
+            value: 2
+        }
+        ,interval: {
+            note: 'Time to wait before launching each session in ms (default:30)',
+            value: 30
+        }
+        ,logfile: {
+            note: 'File to save the log to (default: log.txt)',
+            value: 'log.txt'
+        }
+    });
 
-	options = opts;
-	
-	launch(options);
+    var hOptions = {
+        transport: 'socketio',
+        endpoints: [opts.endpoint]
+    };
 
+    logger.add(logger.transports.File, { filename: opts.logfile });
+
+    logger.info('Launching stress with ' + JSON.stringify(opts));
+
+    launch(opts, hOptions);
 }
 
 // GO!!
